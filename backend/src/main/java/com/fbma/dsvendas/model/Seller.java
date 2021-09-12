573ec408849410d53d2fdcfcadb28f720bb60d36
felipebma <fbma@cin.ufpe.br>
2021-09-12 05:31:53 -0300

    Add database seed and test profile

---
+++ backend/src/main/java/com/fbma/dsvendas/model/Seller.java
@@ -0,0 +1,37 @@
+package com.fbma.dsvendas.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_sellers")
+@NoArgsConstructor
+@Getter
+public class Seller implements Serializable {
+  @Setter
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Setter
+  private String name;
+
+  @OneToMany(mappedBy = "seller")
+  private List<Sale> sales = new ArrayList<>();
+
+  public Seller(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+}
